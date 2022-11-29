f8f080a2aff35d138983201786636786d0cdea24
hossam.shehab <hossam-shehab@hotmail.com>
2022-11-29 20:21:08 +0200

    Modified startup script

--- python/devspace_start.sh
+++ python/devspace_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set +e  # Continue on errors
 
+pip install -r requirements.txt
+
 echo '
 COLOR_CYAN="\033[0;36m"
 COLOR_RESET="\033[0m"
@@ -14,26 +16,25 @@ if [ -z "$BASH" ]; then export PS1="$ "; fi
 # Include projects bin/ folder in PATH
 export PATH="./bin:$PATH"
 
-alias logs="tail -F /.devspace/screenlog.0"' >> ~/.bashrc
+alias logs="tail -F /.devspace/screenlog.0"
+alias restart="PID=$(cat /.devspace/devspace-pid) && rm /.devspace/devspace-pid && kill -9 -$PID"
 
-source ~/.bashrc
-
-echo -e "${COLOR_CYAN}
+echo -e "${COLOR_BLUE}
    ____              ____
   |  _ \  _____   __/ ___| _ __   __ _  ___ ___
-  | | | |/ _ \ \ / /\___ \| '_ \ / _\` |/ __/ _ \\
+  | | | |/ _ \ \ / /\___ \|  _ \ / _\ |/ __/ _ \\
   | |_| |  __/\ V /  ___) | |_) | (_| | (_|  __/
   |____/ \___| \_/  |____/| .__/ \__,_|\___\___|
                           |_|
 ${COLOR_RESET}
-
 Welcome to your development container!
 
 This is how you can work with it:
 - Files will be synchronized between your local machine and this container
-- Some ports will be forwarded, so you can access this container via localhost
-- Run \`${COLOR_BLUE}python main.py${COLOR_RESET}\` to start the application
-"
+- App is running in the background,\`${COLOR_BLUE}ps -ef | grep python${COLOR_RESET}\`
+- Use \`${COLOR_BLUE}logs${COLOR_RESET}\` command to stream App Logs
+- Some ports will be forwarded, so you can access this container via ${COLOR_BLUE}localhost:${LOCAL_PORT}${COLOR_RESET}
+"' >> ~/.bashrc
 
 # Open shell
-bash --norc
+bash
